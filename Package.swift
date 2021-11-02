b786a1206a0ed17cf24c618b65f08313a7d165e3
Felix Herrmann <felix.herrmann@icloud.com>
2021-11-02 16:52:04 +0100

    lowered Mac platform

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftPackageList",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_10),
         .iOS(.v9),
         .tvOS(.v9),
         .watchOS(.v2)
