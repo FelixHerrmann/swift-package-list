7f6690933d78defb1eae72a7e408d3d8ae175d8f
Felix Herrmann <felix.herrmann@icloud.com>
2022-02-28 15:33:15 +0100

    added objc target files

---
+++ Sources/SwiftPackageListObjc/include/SPLError.h
@@ -0,0 +1,22 @@
+//
+//  SPLError.h
+//  SwiftPackageListObjc
+//
+//  Created by Felix Herrmann on 28.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Domain for all Swift Package List errors
+NSErrorDomain const SPLErrorDomain;
+
+/// The possible thrown errors of the SPLPackageList function.
+NS_ERROR_ENUM(SPLErrorDomain, SPLErrorCode) {
+    
+    /// Couldn't find  a package-list.json or package-list.plist file in the specified bundle.
+    SPLErrorNoPackageList = 100,
+};
+
+NS_ASSUME_NONNULL_END
