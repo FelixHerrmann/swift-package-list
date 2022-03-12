946f9179ec32635b2aba1084887e38a14016ee3b
Felix Herrmann <felix.herrmann@icloud.com>
2022-03-12 22:31:20 +0100

    used CleanExit for stopping successfully

--- Sources/SwiftPackageListCommand/SwiftPackageListCommand.swift
+++ Sources/SwiftPackageListCommand/SwiftPackageListCommand.swift
@@ -44,8 +44,7 @@ struct SwiftPackageListCommand: ParsableCommand {
         }
         
         guard FileManager.default.fileExists(atPath: project.packageDotResolvedFileURL.path) else {
-            print("This project has no Swift-Package dependencies")
-            return
+            throw CleanExit.message("This project has no Swift-Package dependencies")
         }
         let packageDotResolved = try String(contentsOfFile: project.packageDotResolvedFileURL.path)
         let packageResolved = try JSONDecoder().decode(PackageResolved.self, from: Data(packageDotResolved.utf8))
@@ -71,13 +70,13 @@ struct SwiftPackageListCommand: ParsableCommand {
             jsonEncoder.outputFormatting = .prettyPrinted
             let json = try jsonEncoder.encode(packages)
             try json.write(to: URL(fileURLWithPath: "\(outputPath)/package-list.json"))
-            print("Generated package-list.json at \(outputPath)")
+            throw CleanExit.message("Generated package-list.json at \(outputPath)")
         case .plist:
             let plistEncoder = PropertyListEncoder()
             plistEncoder.outputFormat = .xml
             let plist = try plistEncoder.encode(packages)
             try plist.write(to: URL(fileURLWithPath: "\(outputPath)/package-list.plist"))
-            print("Generated package-list.plist at \(outputPath)")
+            throw CleanExit.message("Generated package-list.plist at \(outputPath)")
         }
     }
 }
