6832fd9dc19e8e141f86d6baf9fc3dd99a8ae5ad
Felix Herrmann <felix.herrmann@icloud.com>
2022-04-11 15:07:23 +0200

    added findOrganizationName method

--- Sources/SwiftPackageListCommand/Models/Project.swift
+++ Sources/SwiftPackageListCommand/Models/Project.swift
@@ -14,6 +14,21 @@ enum Project {
 
 extension Project {
     
+    init?(path: String) {
+        let fileURL = URL(fileURLWithPath: path)
+        switch fileURL.pathExtension {
+        case "xcodeproj":
+            self = .xcodeproj(fileURL: fileURL)
+        case "xcworkspace":
+            self = .xcworkspace(fileURL: fileURL)
+        default:
+            return nil
+        }
+    }
+}
+
+extension Project {
+    
     var fileURL: URL {
         switch self {
         case .xcodeproj(let fileURL): return fileURL
@@ -33,14 +48,35 @@ extension Project {
 
 extension Project {
     
-    init?(path: String) {
-        let fileURL = URL(fileURLWithPath: path)
-        switch fileURL.pathExtension {
-        case "xcodeproj":
-            self = .xcodeproj(fileURL: fileURL)
-        case "xcworkspace":
-            self = .xcworkspace(fileURL: fileURL)
-        default:
+    func findOrganizationName() -> String? {
+        do {
+            let projectFileURL: URL
+            switch self {
+            case .xcodeproj(let fileURL):
+                projectFileURL = fileURL
+            case .xcworkspace(let fileURL):
+                let contentsURL = fileURL.appendingPathComponent("contents.xcworkspacedata")
+                let contentsString = try String(contentsOf: contentsURL)
+                let contentsRegex = try NSRegularExpression(pattern: "(?<=location = \"group:).*(?=\")")
+                let locationMatches = contentsRegex.matches(in: contentsString, range: NSRange(location: 0, length: contentsString.count))
+                let locationResults = locationMatches
+                    .map { String(contentsString[Range($0.range, in: contentsString)!]) }
+                    .filter { !$0.contains("Pods.xcodeproj") }
+                guard let firstLocation = locationResults.first else {
+                    return nil
+                }
+                projectFileURL = fileURL.deletingLastPathComponent().appendingPathComponent(firstLocation)
+            }
+            
+            let projectURL = projectFileURL.appendingPathComponent("project.pbxproj")
+            let projectString = try String(contentsOf: projectURL)
+            let projectRegex = try NSRegularExpression(pattern: "(?<=ORGANIZATIONNAME = \").*(?=\";)")
+            let organizationMatches = projectRegex.matches(in: projectString, range: NSRange(location: 0, length: projectString.count))
+            let organizationResults = organizationMatches.map { String(projectString[Range($0.range, in: projectString)!]) }
+            
+            return organizationResults.first
+        } catch {
+            print("Warning: Could not find the organization name in your project")
             return nil
         }
     }
