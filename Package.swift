6230618bfbf784f85e09f5a63b91234e137f82bd
Felix Herrmann <felix.herrmann@icloud.com>
2022-05-18 04:10:19 +0200

    convention

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .library(name: "SwiftPackageListUI", targets: ["SwiftPackageListUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
     ],
     targets: [
         .executableTarget(
