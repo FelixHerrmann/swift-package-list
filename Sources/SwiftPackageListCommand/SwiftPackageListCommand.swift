b64ecec466ee4c70a66d6faffff3a2bb3a33b249
Felix Herrmann <felix.herrmann@icloud.com>
2022-08-28 18:14:30 +0200

    bumped version (2.1.1)

--- Sources/SwiftPackageListCommand/SwiftPackageListCommand.swift
+++ Sources/SwiftPackageListCommand/SwiftPackageListCommand.swift
@@ -13,7 +13,7 @@ import SwiftPackageListCore
 struct SwiftPackageListCommand: ParsableCommand {
     
     static var configuration: CommandConfiguration {
-        return CommandConfiguration(version: "2.1.0")
+        return CommandConfiguration(version: "2.1.1")
     }
     
     @Argument(help: "The path to your .xcodeproj or .xcworkspace file.")
