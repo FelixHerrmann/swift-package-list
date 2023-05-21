b6adbe2dfe07f9dd0c703d0a91d50bcf7eb21ed2
Felix Herrmann <felix.herrmann@icloud.com>
2023-05-21 23:21:46 +0200

    Add separate Scan command This required to also create a Generate subcommand for the existing functionality.

---
+++ Sources/SwiftPackageListCommand/SwiftPackageList.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftPackageList.swift
+//  SwiftPackageListCommand
+//
+//  Created by Felix Herrmann on 01.11.21.
+//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct SwiftPackageList: ParsableCommand {
+    static var configuration: CommandConfiguration {
+        return CommandConfiguration(
+            discussion: "A command-line tool to get all used SPM-dependencies of an Xcode project or workspace.",
+            version: "2.2.0",
+            subcommands: [Scan.self, Generate.self]
+        )
+    }
+}
