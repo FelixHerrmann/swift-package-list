dd5623affe639ab6f2c1a3428aabf9952aa53a17
Felix Herrmann <felix.herrmann@icloud.com>
2023-05-29 00:44:12 +0200

    Remove executable name patch

--- Plugins/SwiftPackageListPropertyListPlugin/SwiftPackageListPropertyListPlugin.swift
+++ Plugins/SwiftPackageListPropertyListPlugin/SwiftPackageListPropertyListPlugin.swift
@@ -22,8 +22,6 @@ extension SwiftPackageListPropertyListPlugin: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
         let projectPath = context.xcodeProject.directory.appending("\(context.xcodeProject.displayName).xcodeproj")
         let executable = try context.tool(named: "SwiftPackageListCommand").path
-            .removingLastComponent()
-            .appending("swift-package-list")
         let outputPath = context.pluginWorkDirectory
         let fileType = "plist"
         return [
