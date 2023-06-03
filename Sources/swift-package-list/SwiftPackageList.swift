83f7644c369c826fa4e08973c0bfc5d8f5ad5a71
Felix Herrmann <felix.herrmann@icloud.com>
2023-06-03 16:56:50 +0200

    Bump version (3.0.2)

--- Sources/swift-package-list/SwiftPackageList.swift
+++ Sources/swift-package-list/SwiftPackageList.swift
@@ -13,7 +13,7 @@ struct SwiftPackageList: ParsableCommand {
     static var configuration: CommandConfiguration {
         return CommandConfiguration(
             discussion: "A command-line tool to get all used SPM-dependencies of an Xcode project or workspace.",
-            version: "3.0.1",
+            version: "3.0.2",
             subcommands: [Scan.self, Generate.self]
         )
     }
