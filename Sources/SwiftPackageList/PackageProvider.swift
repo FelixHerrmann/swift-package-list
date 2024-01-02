572993a6dffa3e193dc720186f45128ed6df3a96
Felix Herrmann <felix.herrmann@icloud.com>
2024-01-02 22:07:44 +0100

    Replace top-level packageList function with PackageProvider protocol

---
+++ Sources/SwiftPackageList/PackageProvider.swift
@@ -0,0 +1,15 @@
+//
+//  PackageProvider.swift
+//  SwiftPackageList
+//
+//  Created by Felix Herrmann on 02.01.24.
+//
+
+/// A type that provides ``Package``s from some arbitrary source.
+public protocol PackageProvider {
+    
+    /// Provides the array of packages.
+    /// - Returns: An array of ``Package`` objects.
+    /// - Throws: This method can throw if something went wrong during the process.
+    func packages() throws -> [Package]
+}
---
+++ Sources/SwiftPackageList/PropertyListPackageProvider.swift
@@ -0,0 +1,51 @@
+//
+//  PropertyListPackageProvider.swift
+//  SwiftPackageList
+//
+//  Created by Felix Herrmann on 02.01.24.
+//
+
+import Foundation
+
+/// A package provider for `.plist` package-lists stored in a `Bundle`.
+public struct PropertyListPackageProvider: PackageProvider {
+    
+    /// The bundle to read the Property List file from.
+    ///
+    /// Default value is `Bundle.main`.
+    public var bundle: Bundle = .main
+    
+    /// The name of the Property List file, usually specified with the `--custom-file-name` option.
+    ///
+    ///  Default value is `"package-list"`.
+    public var fileName = "package-list"
+    
+    /// Creates a Property List package provider.
+    /// - Parameters:
+    ///   - bundle: The bundle to read the Property List file from.
+    ///   - fileName: The name of the Property List file.
+    public init(bundle: Bundle = .main, fileName: String = "package-list") {
+        self.bundle = bundle
+        self.fileName = fileName
+    }
+    
+    public func packages() throws -> [Package] {
+        guard let path = bundle.path(forResource: fileName, ofType: "plist") else {
+            throw CocoaError(.fileNoSuchFile, userInfo: ["file_name": fileName, "file_type": "plist"])
+        }
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        let decoder = PropertyListDecoder()
+        return try decoder.decode([Package].self, from: data)
+    }
+}
+
+extension PackageProvider where Self == PropertyListPackageProvider {
+    /// A Property List package provider.
+    /// - Parameters:
+    ///   - bundle: The bundle to read the Property List file from.
+    ///   - fileName: The name of the Property List file.
+    /// - Returns: A ``PropertyListPackageProvider`` object.
+    public static func propertyList(bundle: Bundle = .main, fileName: String = "package-list") -> Self {
+        return PropertyListPackageProvider(bundle: bundle, fileName: fileName)
+    }
+}
